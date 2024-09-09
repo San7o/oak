3c38ef9db2d363656a1015feec2cab8f063141f9
San7o <santigio2003@gmail.com>
2024-09-09 21:24:57 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# oak - your favourite logger
+
+This is a simple, header-only, C++23 logger. It features:
+
+- [x] multiple logging levels
+
+- [x] log to file
+
+- [ ] async logging and multithreading
+
+## Current state
+
+TODO
---
+++ include/oak.hpp
@@ -0,0 +1,220 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Giovanni Santini
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#pragma once
+
+#include "utils_types.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <ctime>
+#include <ostream>
+#include <iomanip>
+
+#define DEBUG(...)                                                             \
+    brenta::logger::log(brenta::types::log_level::DEBUG, __VA_ARGS__)
+#define INFO(...)                                                              \
+    brenta::logger::log(brenta::types::log_level::INFO, __VA_ARGS__)
+#define WARNING(...)                                                           \
+    brenta::logger::log(brenta::types::log_level::WARNING, __VA_ARGS__)
+#define ERROR(...)                                                             \
+    brenta::logger::log(brenta::types::log_level::ERROR, __VA_ARGS__)
+
+namespace oak
+{
+
+enum log_level
+{
+    DEBUG = 0,
+    INFO,
+    WARNING,
+    ERROR,
+    OUTPUT,
+    DISABLED
+};
+
+std::ostream &operator<<(std::ostream &os, const log_level level);
+
+/**
+ * @brief Logger class
+ *
+ * This class is used to log messages to the console and to a log file. The log
+ * messages can be filtered by log level.
+ */
+class logger
+{
+  public:
+    /**
+     * @brief Log level
+     *
+     * The log level is used to filter the messages that are printed to the
+     * console and to the log file. The log level can be set using the
+     * SetLogLevel method. Onlt the messages with a Loglevel greater or equal to
+     * the set log level are printed.
+     */
+    static brenta::types::log_level level;
+    /**
+     * @brief Log file
+     *
+     * The log file is used to store the messages that are printed to the
+     * console. The log file can be set using the SetLogFile method.
+     */
+    static std::ofstream log_file;
+
+    /**
+     * @brief Initialize the logger
+     *
+     * This method initializes the logger. It should be called before any other
+     * method of the logger is called.
+     */
+    static void init();
+    /**
+     * @brief Set the log level
+     *
+     * This method sets the log level. The log level is used to filter the
+     * messages that are printed to the console and to the log file. The log
+     * level can be set to DEBUG, INFO, WARNING, ERROR, or DISABLED.
+     *
+     * @param level The log level
+     */
+    static void set_log_level(brenta::types::log_level level);
+    /**
+     * @brief Set the log file
+     *
+     * This method sets the log file. The log file is used to store the messages
+     * that are printed to the console. The log file can be set to a file name.
+     *
+     * @param file The log file
+     */
+    static void set_log_file(const std::string &file);
+    /**
+     * @brief Close the logger
+     *
+     * This method closes the logger. It should be called before the program
+     * exits.
+     */
+    static void close();
+
+    /**
+     * @brief Log a message to stdout
+     *
+     * This method logs a message on stdout with the given log level. The
+     * message can be any number of arguments of any type.
+     *
+     * @param level The log level
+     * @param args The message
+     */
+    template <typename T, typename... Args>
+    static void log_to_stdout(T message, Args... args)
+    {
+        std::cout << message;
+        log_to_stdout(args...);
+    }
+
+    /**
+     * @brief Log a message to the log file
+     *
+     * This method logs a message with the given log level. The message can be
+     * any number of arguments of any type. The message is printed to the log
+     * file if the log file is open.
+     *
+     * @param level The log level
+     * @param args The message
+     */
+    template <typename T, typename... Args>
+    static void log_to_file(T message, Args... args)
+    {
+        log_file << message;
+        log_to_file(args...);
+    }
+    /**
+     * @brief Log a message
+     *
+     * This method logs a message with the given log level. The message can be
+     * any number of arguments of any type. The message is printed to the
+     * console and to the log file if the log file is open.
+     *
+     * @param level The log level
+     * @param args The message
+     */
+    template <typename... Args>
+    static void log(brenta::types::log_level level, Args... args)
+    {
+        if (logger::level > level)
+            return;
+        log_to_stdout(level, ": ", args...);
+        if (logger::log_file.is_open())
+            log_to_file(level, ": ", args...);
+    }
+
+  private:
+    /* Base cases */
+    template <typename T> static void log_to_stdout(T message)
+    {
+        std::cout << message << std::endl;
+    }
+    template <typename T> static void log_to_file(T message)
+    {
+        log_file << message << std::endl;
+    }
+};
+
+oak::log_level logger::level = oak::log_level::WARNING;
+std::ofstream logger::log_file;
+
+void logger::set_log_level(oak::types::log_level level)
+{
+    logger::level = level;
+}
+
+void logger::init()
+{
+    set_log_file("logs/log.txt");
+}
+
+void logger::close()
+{
+    logger::log_file.close();
+}
+
+void logger::set_log_file(const std::string &file)
+{
+    logger::log_file.open(file, std::ios::app);
+    if (!logger::log_file.is_open())
+    {
+        std::cout << "Error: Could not open log file" << std::endl;
+    }
+
+    auto now = std::chrono::system_clock::now();
+    auto now_time_t = std::chrono::system_clock::to_time_t(now);
+    std::tm now_tm = *std::localtime(&now_time_t);
+    log_file << "----------" << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S")
+             << "----------" << std::endl;
+}
+
+} // namespace oak
